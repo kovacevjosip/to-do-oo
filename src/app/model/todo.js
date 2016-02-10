996f4cda159663284835f8a867ace1dfcb2cdc58
kovacevic.josip <kovacevic.josip@nsoft.ba>
2016-02-11 00:03:50 +0100

    Make list items deletable

--- src/app/model/todo.js
+++ src/app/model/todo.js
@@ -9,16 +9,16 @@ function ToDo() {
 
 // Reset list
 ToDo.prototype.reset = function () {
-    this.list = [];
-    this.listCount = 0;
+    this.itemList = [];
+    this.itemListCount = 0;
 };
 
 // Add new item
 ToDo.prototype.addItem = function (name, type) {
-    var item = new Item(this.listCount++, name, type); // Create new object
+    var item = new Item(this.itemListCount++, name, type); // Create new object
     
     // Add object to array and render
-    this.list.push(item);
+    this.itemList.push(item);
     this.ui.renderListItem(item);
 };
 
--- src/app/model/ui.js
+++ src/app/model/ui.js
@@ -35,27 +35,80 @@ UI.prototype.bindClickListeners = function () {
     });
 };
 
+// Create element; name: string, (value: string)
+UI.prototype.createElement = function (name, value) {
+    var el = document.createElement(name);
+    if (value) {
+        el.innerHTML = value;
+    }
+
+    return el;
+};
+
 // Render new item
 UI.prototype.renderListItem = function (item) {
-    var li, p, span;
-
-    // Create element; name: string, (value: string)
-    function createElement(name, value) {
-        var el = document.createElement(name);
-        if (value) {
-            el.innerHTML = value;
-        }
-
-        return el;
-    }
+    var self = this,
+        li, p, span;
     
     // Create list element
-    li = createElement('LI');
-    p = createElement('P', item.name);
-    span = createElement('SPAN', item.type + ' task ' + item.id);
+    li = this.createElement('LI');
+    p = this.createElement('P', item.name);
+    span = this.createElement('SPAN', item.type + ' task ' + item.id);
+    
+    // Bind click listener on list item
+    li.addEventListener('click', function (e) {
+        self.settingsBuilder(this, item);
+    });
 
     // Append list element in DOM
     p.appendChild(span);
     li.appendChild(p);
     this.elements.ul.appendChild(li);
+};
+
+// Edit, delete or move to done
+UI.prototype.updateListItem = function (type, topElement, liElement, item) {
+    switch (type) {
+        case 'EDIT':
+            break;
+        case 'DONE':
+            break;
+        case 'DELETE':
+            // TODO: make method in todo.js that will delete element from array
+            // pass only one parameter, item ID
+            liElement.parentNode.removeChild(liElement);
+            break;
+        default:
+            break;
+    }
+
+    topElement.style.display = 'none';
+    console.log(type);
+    console.log(todo.itemList);
+};
+
+// Build HTML element on top list item
+UI.prototype.settingsBuilder = function (liElement, item) {
+    var self = this,
+        rootDiv = this.createElement('DIV'),
+        span1 = this.createElement('SPAN', 'EDIT'),
+        span2 = this.createElement('SPAN', 'DONE'),
+        span3 = this.createElement('SPAN', 'DELETE');
+    
+    // Append spans to div
+    rootDiv.appendChild(span1);
+    rootDiv.appendChild(span2);
+    rootDiv.appendChild(span3);
+
+    rootDiv.addEventListener('click', function (e) {
+        // Stop event propagation
+        e.stopPropagation();
+
+        var type = e.target.tagName === 'SPAN' ? e.target.textContent : '';
+
+        if (type) self.updateListItem(type, rootDiv, liElement, item);
+    });
+
+    // Append div to list element
+    liElement.appendChild(rootDiv);
 };
